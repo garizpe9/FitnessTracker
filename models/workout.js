33d07face33a667c90e753ae567fe98fe9ab7735
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-25 19:05:14 -0500

    adding models

---
+++ models/workout.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  type: {
+      type:String,
+      trim: true,
+      required: "Please include excercise type"
+     },
+  name: {
+      type: String,
+      trim: true,
+      required: "Please include name of exercise"
+      },
+  duration: {
+      type: Number,
+      trim: true,
+      required: "Please include amount of minutes"
+      },
+  weight: Number,
+  reps: Number,
+  sets: Number
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
