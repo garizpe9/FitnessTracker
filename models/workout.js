9a4dc7896aedea341092f2c80c0898e3c4a0c53b
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-26 20:02:08 -0500

    spellchecking th db

--- models/workout.js
+++ models/workout.js
@@ -6,27 +6,26 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    excercises:[
-    {
-        type: {
-            type:String,
-            trim: true,
-            required: "Please include excercise type"
+    exercises:[
+        {
+            type: {
+                type:String,
+                trim: true,
+                required: "Please include excercise type"
         },
-        name: {
-            type: String,
-            trim: true,
-            required: "Please include name of exercise"
+            name: {
+                type: String,
+                trim: true,
+                required: "Please include name of exercise"
         },
-
-        duration: {
-            type: Number,
-            trim: true,
-            required: "Please include amount of minutes"
+            duration: {
+                type: Number,
+                trim: true,
+                required: "Please include amount of minutes"
         },
-        weight: Number,
-        reps: Number,
-        sets: Number
+            weight: Number,
+            reps: Number,
+            sets: Number
         }
     ]
 });
