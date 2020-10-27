4339ae7ec424fc55be9f9855fcb3bc973a8f9087
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-26 20:21:58 -0500

    json

--- models/workout.js
+++ models/workout.js
@@ -12,23 +12,31 @@ const WorkoutSchema = new Schema({
                 type:String,
                 trim: true,
                 required: "Please include excercise type"
-        },
+             },
             name: {
                 type: String,
                 trim: true,
                 required: "Please include name of exercise"
-        },
+            },
             duration: {
                 type: Number,
                 trim: true,
                 required: "Please include amount of minutes"
-        },
+            },
             weight: Number,
             reps: Number,
             sets: Number
         }
     ]
-});
+    },
+    { 
+        toJSON: {
+        virtual:true
+        }
+    }
+);
+
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
