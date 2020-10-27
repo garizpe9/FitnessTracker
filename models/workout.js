65473e01394f4049941a8fd5b1b510dfa7d1cbda
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-26 20:37:11 -0500

    updating schema

--- models/workout.js
+++ models/workout.js
@@ -2,39 +2,57 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day:{
+    
+    day: {
         type: Date,
-        default: Date.now
+        default: Date.now(),
+        unique: true
     },
-    exercises:[
+    exercises: [
         {
-            type: {
-                type:String,
-                trim: true,
-                required: "Please include excercise type"
-             },
-            name: {
-                type: String,
-                trim: true,
-                required: "Please include name of exercise"
-            },
-            duration: {
-                type: Number,
-                trim: true,
-                required: "Please include amount of minutes"
-            },
-            weight: Number,
-            reps: Number,
-            sets: Number
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            require: "Enter name of exercise"
+        },
+        duration: {
+            type: Number,
+            required: "Enter the duration"
+        },
+        weight: {
+            type: Number,
+            required: "Number is required"
+        },
+        reps: {
+            type: Number,
+            required: "Number is required"
+        },
+        sets: {
+            type: Number,
+            required: "Number is required"
+        },
+        distance: {
+            type: Number,
+            require: "Number is requried"
+        }
         }
     ]
     },
-    { 
-        toJSON: {
-        virtual:true
-        }
+    {
+    toJSON: {
+        virtuals: true
+    }
     }
 );
+
+// To include virtuals in res.json(), you need to set the toJSON schema option to { virtuals: true }.
+
+// https://mongoosejs.com/docs/2.7.x/docs/virtuals.html
 
 
 
