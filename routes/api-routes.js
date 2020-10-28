99732bb33b541332d105daa496b3af27a97b7dee
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-27 21:53:49 -0500

    updating api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 // "/api/workouts "- get (workout)/ update/ post
 var db = require("../models")
+const mongojs = require("mongojs");
 
 module.exports = function(app) {
  app.get("/api/workouts/range", (req, res) => {
@@ -33,9 +34,17 @@ module.exports = function(app) {
       });
   });
 
-  app.put("/api/workouts/:id", (req, res) => {
+  app.post("/api/workouts/:id", (req, res) => {
     console.log(req)
-    db.Workout.updateOne({_id: req.params.id,}, {exercises: req.body.exercises})
+    db.Workout.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {$set: {
+        exercises: req.body.exercises
+        }
+      } 
+      )
      console.log(req)
       .then(data => {
         res.json(data);
